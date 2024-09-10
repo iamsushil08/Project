d0ab78ff937d0b2bba7f3e5ba57b0bffcba9b44d
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-09-10 22:15:09 +0545

    changes by sandhya

--- frontend/backend/insert.php
+++ frontend/backend/insert.php
@@ -9,14 +9,20 @@ if(isset($_POST["submit"])){
     $password=$_POST["password"];
     $cpassword=$_POST["cpassword"];
 
+    if ($password !== $cpassword) {
+        echo "Passwords do not match!";
+  
+    }
+
 
     $password_hashed= password_hash($password, PASSWORD_DEFAULT);
     $sql="INSERT INTO users(firstname,lastname,email,phone,password) VALUES ('$fname','$lname','$email','$phone','$password_hashed')";
     $result=mysqli_query($conn,$sql);
-    if(!$result){
-        echo "Unsuccessful";
+    if($result){
+       header("Location:../index.html");
 
     }
+
   
 }
 
