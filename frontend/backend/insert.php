0eb330e500839bbd840b9e878630b065c965b0e8
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-09-19 06:59:10 +0545

    changes by sandhya

--- frontend/backend/insert.php
+++ frontend/backend/insert.php
@@ -17,12 +17,12 @@ if(isset($_POST["submit"])){
     $sql="INSERT INTO users(firstname,lastname,email,phone,password) VALUES ('$fname','$lname','$email','$phone','$password_hashed')";
     $result=mysqli_query($conn,$sql);
     if($result){
-    //    header("Location:../index.html");
-    //    exit();
-   echo" you are logged in";
+       header("Location:../index.html");
+       exit();
+//    echo" you are logged in";
 
     }
-    else{
+    else{ 
         // echo"Not done"; 
         echo "Error: " . mysqli_error($conn); 
     }
