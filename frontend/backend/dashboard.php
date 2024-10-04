add07f3b418448fc05741f2db84050f5ea20aae3
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-10-04 15:56:09 +0545

    changes by sandhya

--- frontend/backend/dashboard.php
+++ frontend/backend/dashboard.php
@@ -1,25 +1,22 @@
 <?php
 session_start();
 if (!isset($_SESSION['admin_id'])) {
-    header('Location: admin_login.php');
+    header('Location:alogin.php');
     exit;
 }
+?>
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    include 'connection.php';
+<html>
+<h2>Admin Dashboard</h2>
+<button><a href=".div1" style="text-decoration:none">Add Car</a></button>
+<button><a href="" style="text-decoration:none">Delete Car </a></button>
 
-    $model = $_POST['model'];
-    $brand = $_POST['brand'];
-    $year = $_POST['year'];
-    $price_per_day = $_POST['price_per_day'];
-    $availability = 'available';
 
-    $query = "INSERT INTO cars (model, brand, year, price_per_day, availability)
-              VALUES ('$model', '$brand', '$year', '$price_per_day', '$availability')";
 
-    if (mysqli_query($conn, $query)) {
-        echo "Car added successfully.";
-    } else {
-        echo "Error: " . mysqli_error($conn);
-    }
-}
+<div class="div1">
+    <form action="" method="POST">
+        hello
+    </form>
+</div>
+
+</html>
