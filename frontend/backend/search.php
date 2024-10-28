5c357a2e15c0565d3601f971c23a23899e6dfb81
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-10-28 19:56:58 +0545

    changes by sandhya

--- frontend/backend/search.php
+++ frontend/backend/search.php
@@ -6,16 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-
-
-
-
-
-
-
-
-
-
+    /* do internal here */
     </style>
 
 </head>
