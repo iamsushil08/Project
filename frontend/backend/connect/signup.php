eed9a96c85b303a0367a640a0e7f614c6c3be976
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-12-17 13:35:10 +0545

    changes by sandhya

--- frontend/backend/connect/signup.php
+++ frontend/backend/connect/signup.php
@@ -12,7 +12,7 @@ if (isset($_POST['signin'])) {
             $user = mysqli_fetch_assoc($result);
             if (password_verify($password, $user['password'])) {
                 $_SESSION['email'] = $user['email'];
-                header("Location: ../index.php");
+                header("Location:../..index.php");
                 exit;
             } else {
                 $error = "Incorrect username or password.";
