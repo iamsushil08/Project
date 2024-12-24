580982f65fb94fef602d5c1e8c74a40ee0f04acf
iamsushil08 <iamsushil01@gmail.com>
2024-12-24 21:39:18 +0545

    changes made by sushil

--- frontend/bookings/finalbooking.php
+++ frontend/bookings/finalbooking.php
@@ -43,35 +43,83 @@ if ($result && mysqli_num_rows($result) > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Final Booking</title>
     <style>
-    body {
+    /* body { 
         margin: 0px;
         padding: 0px;
         background-color: red;
         display: flex;
         align-items: right;
         justify-content: center;
-    }
+    /* } */
 
     .box {
-        height: 400px;
-        width: 300px;
-        background-color: white;
-        color: black;
+        height: 390px;
+        width: 450px;
+         /* background-color:red; */
+        color:black;
+        margin-top:40px;
+        margin-left:475px;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
     }
+    #heading{
+        margin-left:113px;
+    }
+    #heading2{
+        margin-left:153px;
+    }
+    #name{
+        margin-left:53px;
+        margin-top:2px;
+    }
+    #description {
+        margin-left:53px;
+    }
+    #color{
+        margin-left:53px;
+    }
+    #hour{
+        margin-left:53px;
+    
+    }
+    #totalpayment{
+        margin-left:53px;
+    }
+    #downpayment{
+        margin-left:53px;
+    }
+    #paypal-button-container{
+        margin-left:557px;
+        width:315px;
+        border-radius:12px;
+        margin-top:22px;
+    }
+    /* #smart-button-container{ */
+        /* height:333px; */
+        /* width: 666px; */
+        /* background-color: red; */
+
+    /* } */
+    /* #smart{ */
+        /* style="text-align: center; */
+        /* margin-left:33px; */
+        /* width: 559px; */
+
+    /* } */
     </style>
 </head>
 
 <body>
 
     <div class="box">
-        <h1>Final Booking</h1>
-        <h2>Car Details</h2>
-        <p><strong>Name:</strong> <?php echo htmlspecialchars($car['name']); ?></p>
-        <p><strong>Description:</strong> <?php echo htmlspecialchars($car['description']); ?></p>
-        <p><strong>Color:</strong> <?php echo htmlspecialchars($car['color']); ?></p>
-        <p><strong>Charge/Hour:</strong> <?php echo htmlspecialchars($car['charge']); ?></p>
-        <p><strong>Total Payment:</strong> <?php echo number_format($totalpayment, 2); ?></p>
-        <p><strong>Down Payment (15%):</strong> <?php echo number_format($downpay, 2); ?></p>
+        <h1 id="heading">Final Booking</h1>
+        <h2 id="heading2">Car Details</h2>
+        <p id="name"><strong>Name:</strong> <?php echo htmlspecialchars($car['name']); ?></p>
+        <p id="description" ><strong>Description:</strong> <?php echo htmlspecialchars($car['description']); ?></p>
+        <p id="color" ><strong>Color:</strong> <?php echo htmlspecialchars($car['color']); ?></p>
+        <p id="hour"><strong>Charge/Hour:</strong> <?php echo htmlspecialchars($car['charge']); ?></p>
+        <p id="totalpayment"><strong>Total Payment:</strong> <?php echo number_format($totalpayment, 2); ?></p>
+        <p id="downpayment"><strong>Down Payment (15%):</strong> <?php echo number_format($downpay, 2); ?></p>
     </div>
     <!-- <div class="box2">
 
@@ -83,11 +131,11 @@ if ($result && mysqli_num_rows($result) > 0) {
     </div> -->
 
 
-    <div id=" smart-button-container">
-        <div style="text-align: center;">
+    <!-- <div id=" smart-button-container"> -->
+        <!-- <div id="smart" > -->
             <div id="paypal-button-container"></div>
-        </div>
-    </div>
+        <!-- </div> -->
+    <!-- </div> -->
 
     <script
         src="https://www.paypal.com/sdk/js?client-id=AfrxsugjDF5UzPrGu9K5OWvVkWDIbNIZ9prfcwu1lOhZNk_b7Qb80O79G60MO92hOk7S_BMAGfMj_lGp&currency=USD"
@@ -97,11 +145,11 @@ if ($result && mysqli_num_rows($result) > 0) {
     function initPayPalButton() {
         paypal.Buttons({
             style: {
-                shape: 'rect',
-                color: 'gold',
-                layout: 'vertical',
-                label: 'paypal',
-            },
+                 shape: 'rect',
+                 color: 'gold',
+                 layout: 'vertical',
+                 label: 'paypal',
+             },
 
             createOrder: function(data, actions) {
                 return actions.order.create({
